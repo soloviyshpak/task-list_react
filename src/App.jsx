78c7e6aef9131050063af38b44d5c79f05124eed
Artyom Soloveyov <soloviy.shpak16@gmail.com>
2025-09-26 22:40:51 +0300

    Задачи сохраняются и добавляются в массив

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 
 function App() {
+  const [tasks, setTasks] = useState([]);
   const [openSection, setOpenSection] = useState({
     taskList: false,
     tasks: true,
@@ -14,6 +15,12 @@ function App() {
     }));
   }
 
+  function addTask(task) {
+    setTasks([...tasks, { ...task, completed: false, id: Date.now() }]);
+  }
+
+  console.log(tasks);
+
   return (
     <div className="app">
       <div className="task-container">
@@ -24,7 +31,7 @@ function App() {
         >
           +
         </button>
-        {openSection.taskList && <TaskForm />}
+        {openSection.taskList && <TaskForm addTask={addTask} />}
       </div>
       <div className="task-container">
         <h2>Tasks</h2>
@@ -55,16 +62,41 @@ function App() {
   );
 }
 
-function TaskForm() {
+function TaskForm({ addTask }) {
+  const [title, setTitle] = useState();
+  const [priority, setPriority] = useState('Low');
+  const [deadline, setDeadline] = useState();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (title.trim() && deadline) {
+      addTask({ title, priority, deadline });
+    }
+    setTitle('');
+    setPriority('Low');
+    setDeadline('');
+  }
+
   return (
-    <form action="" className="task-form">
-      <input type="text" value={''} placeholder="Task title" required />
-      <select value={''}>
+    <form action="" className="task-form" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={title}
+        placeholder="Task title"
+        required
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <select value={priority} onChange={(e) => setPriority(e.target.value)}>
         <option value="High">High</option>
         <option value="Medium">Medium</option>
         <option value="Low">Low</option>
       </select>
-      <input type="datetime-local" required value={''} />
+      <input
+        type="datetime-local"
+        required
+        value={deadline}
+        onChange={(e) => setDeadline(e.target.value)}
+      />
       <button type="submit">Add task</button>
     </form>
   );
